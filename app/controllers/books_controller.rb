8f68e74983cd533bd316ca4fd3210bed6769b723
azarJY <143927567+azarJY@users.noreply.github.com>
2023-09-21 13:14:54 +0000

    [middle]4-15課題2destroy実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,14 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
-  
-  def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/books/show'
-  end
 
   def index
     @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def show
@@ -18,11 +16,24 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
   
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
